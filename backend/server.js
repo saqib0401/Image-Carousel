324b568f3fdbfb0f75f93b5d5ee6716170f8c98c
Saqib Shaikh <saqib2001shaikh@outlook.com>
2021-08-27 17:09:39 +0530

    Setting up frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => res.send("Api is running"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
